9b59017ccdae10c0e9e65fe9a395f079f35b3f00
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-16 10:36:44 +0200

    Profile colour on blurbs

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -4,12 +4,12 @@
         <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
         @foreach ($myBlurbs as $blurbs)
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4" style="max-width: 416px;">
-                <div class="rh-blurb mx1 my2">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579;">
                     <div style="height: 195px; overflow:hidden;position:relative;">
                         <img src="{!! $blurbs['image_url'] !!}" style="height:100%;">
                     </div>
                     <div class="rh-blurb__description mx2 pt2">
-                        <h3 style="line-height: 1.3"><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black;">{{ $blurbs['link_title'] }}</a></h3>
+                        <h3 style="line-height: 1.3"><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="  color:black;">{{ $blurbs['link_title'] }}</a></h3>
                         <p style="line-height: 1.4">{{ $blurbs['post_content'] }}</p>
                     </div>
                 </div>
