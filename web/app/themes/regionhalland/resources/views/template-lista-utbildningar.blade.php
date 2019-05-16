063b30c41408ad95d7969c19ac3c619da1d513a7
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-16 17:18:41 +0200

    All types of education listed. Layout almost done

--- web/app/themes/regionhalland/resources/views/template-lista-utbildningar.blade.php
+++ web/app/themes/regionhalland/resources/views/template-lista-utbildningar.blade.php
@@ -14,33 +14,7 @@
                 <ul class="rh-education-list">
                     @php($myItems = get_region_halland_acf_page_education_repeater_items())
 
-                    @if($myItems['vard_omsorg'])
-                        <li class="pt3 pb2 rh-education-list__item">
-                            <h2 class="pb2" style="width:100%; text-align:center;">Vård och omsorg</h2>
-                            <ul class="mb4 mx-auto" style="max-width:50em;">
-                                @foreach($myItems['vard_omsorg'] as $item)
-                                    <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
-                                        <div class="col col-6">
-                                            <strong>
-                                                {{ $item['page']->education_name }}
-                                            </strong>
-                                        </div>
-                                        <div class="col col6">
-                                            <ul aria-label="Utbildningsorter för {{ $item['page']->education_name }}">
-                                                @foreach ($item['page']->metadata as $metadata)
-                                                    <li style="display:inline;">
-                                                        <a class="rh-labels" style="text-decoration:none;background: #E99579;" href="{{ $metadata['link_url'] }}">
-                                                            {{ $metadata['kommun_name'] }}
-                                                        </a>
-                                                    </li>
-                                                @endforeach
-                                            </ul>
-                                        </div>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </li>
-                    @endif
+
 
                     @if($myItems['barn_fritid'])
                         <li class="pt3 pb2 rh-education-list__item">
@@ -154,6 +128,68 @@
                         </li>
                     @endif
 
+
+
+
+
+                    @if($myItems['handel_administration'])
+                        <li class="pt3 pb2 rh-education-list__item">
+                            <h2 class="pb2" style="width:100%; text-align:center;">Handel och administration</h2>
+                            <ul class="mb4 mx-auto" style="max-width:50em;">
+                                @foreach($myItems['handel_administration'] as $item)
+                                    <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
+                                        <div class="col col-6">
+                                            <strong>
+                                                {{ $item['page']->education_name }}
+                                            </strong>
+                                        </div>
+                                        <div class="col col6">
+                                            <ul aria-label="Utbildningsorter för {{ $item['page']->education_name }}">
+                                                @foreach ($item['page']->metadata as $metadata)
+                                                    <li style="display:inline;">
+                                                        <a class="rh-labels" style="text-decoration:none;background: #E99579;" href="{{ $metadata['link_url'] }}">
+                                                            {{ $metadata['kommun_name'] }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
+
+
+                    @if($myItems['hantverk'])
+                        <li class="pt3 pb2 rh-education-list__item">
+                            <h2 class="pb2" style="width:100%; text-align:center;">Hantverk</h2>
+                            <ul class="mb4 mx-auto" style="max-width:50em;">
+                                @foreach($myItems['hantverk'] as $item)
+                                    <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
+                                        <div class="col col-6">
+                                            <strong>
+                                                {{ $item['page']->education_name }}
+                                            </strong>
+                                        </div>
+                                        <div class="col col6">
+                                            <ul aria-label="Utbildningsorter för {{ $item['page']->education_name }}">
+                                                @foreach ($item['page']->metadata as $metadata)
+                                                    <li style="display:inline;">
+                                                        <a class="rh-labels" style="text-decoration:none;background: #E99579;" href="{{ $metadata['link_url'] }}">
+                                                            {{ $metadata['kommun_name'] }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
                     @if($myItems['industriteknik'])
                         <li class="pt3 pb2 rh-education-list__item">
                             <h2 class="pb2" style="width:100%; text-align:center;">Industriteknik</h2>
@@ -182,11 +218,95 @@
                         </li>
                     @endif
 
+                    @if($myItems['naturbruk'])
+                        <li class="pt3 pb2 rh-education-list__item">
+                            <h2 class="pb2" style="width:100%; text-align:center;">Naturbruk</h2>
+                            <ul class="mb4 mx-auto" style="max-width:50em;">
+                                @foreach($myItems['naturbruk'] as $item)
+                                    <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
+                                        <div class="col col-6">
+                                            <strong>
+                                                {{ $item['page']->education_name }}
+                                            </strong>
+                                        </div>
+                                        <div class="col col6">
+                                            <ul aria-label="Utbildningsorter för {{ $item['page']->education_name }}">
+                                                @foreach ($item['page']->metadata as $metadata)
+                                                    <li style="display:inline;">
+                                                        <a class="rh-labels" style="text-decoration:none;background: #E99579;" href="{{ $metadata['link_url'] }}">
+                                                            {{ $metadata['kommun_name'] }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
                     @if($myItems['restaurang_livsmedel'])
                         <li class="pt3 pb2 rh-education-list__item">
                             <h2 class="pb2" style="width:100%; text-align:center;">Restaurang och livsmedel</h2>
                             <ul class="mb4 mx-auto" style="max-width:50em;">
                                 @foreach($myItems['restaurang_livsmedel'] as $item)
+                                    <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
+                                        <div class="col col-6">
+                                            <strong>
+                                                {{ $item['page']->education_name }}
+                                            </strong>
+                                        </div>
+                                        <div class="col col6">
+                                            <ul aria-label="Utbildningsorter för {{ $item['page']->education_name }}">
+                                                @foreach ($item['page']->metadata as $metadata)
+                                                    <li style="display:inline;">
+                                                        <a class="rh-labels" style="text-decoration:none;background: #E99579;" href="{{ $metadata['link_url'] }}">
+                                                            {{ $metadata['kommun_name'] }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
+                    @if($myItems['vard_omsorg'])
+                        <li class="pt3 pb2 rh-education-list__item">
+                            <h2 class="pb2" style="width:100%; text-align:center;">Vård och omsorg</h2>
+                            <ul class="mb4 mx-auto" style="max-width:50em;">
+                                @foreach($myItems['vard_omsorg'] as $item)
+                                    <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
+                                        <div class="col col-6">
+                                            <strong>
+                                                {{ $item['page']->education_name }}
+                                            </strong>
+                                        </div>
+                                        <div class="col col6">
+                                            <ul aria-label="Utbildningsorter för {{ $item['page']->education_name }}">
+                                                @foreach ($item['page']->metadata as $metadata)
+                                                    <li style="display:inline;">
+                                                        <a class="rh-labels" style="text-decoration:none;background: #E99579;" href="{{ $metadata['link_url'] }}">
+                                                            {{ $metadata['kommun_name'] }}
+                                                        </a>
+                                                    </li>
+                                                @endforeach
+                                            </ul>
+                                        </div>
+                                    </li>
+                                @endforeach
+                            </ul>
+                        </li>
+                    @endif
+
+                    @if($myItems['ovrigt'])
+                        <li class="pt3 pb2 rh-education-list__item">
+                            <h2 class="pb2" style="width:100%; text-align:center;">Övrigt</h2>
+                            <ul class="mb4 mx-auto" style="max-width:50em;">
+                                @foreach($myItems['ovrigt'] as $item)
                                     <li class="clearfix pt2 pb1" style="border-bottom: 1px solid #D1D1D1;">
                                         <div class="col col-6">
                                             <strong>
