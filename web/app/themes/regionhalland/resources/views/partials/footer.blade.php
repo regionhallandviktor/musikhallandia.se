2ff78bb1a8f5904daf5e1c3bcfc292d8e6632e42
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-16 09:13:25 +0200

    Removed google tag manager from regionhalland.se

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -167,8 +167,8 @@
 </script>
 <script type="text/javascript">var _baTheme=0, _baMode='Aktivera Talande Webb', _baUseCookies=true, _baHideOnLoad=true;</script>
 <script type="text/javascript" src="//www.browsealoud.com/plus/scripts/ba.js"></script>
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1181886-12"></script>
+
+
 <script>
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
