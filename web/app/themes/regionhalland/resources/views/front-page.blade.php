a1a8ba020d4529ef6add0ae51128d72b11856ee9
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-16 10:36:16 +0200

    New colour for front page blurbs

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -6,7 +6,7 @@
         <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
 
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="rh-blurb mx1 my2">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579">
                     <div style="height: 200px; overflow:hidden;position:relative">
                         <img src="{!! $myBlurbs[0]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
@@ -18,7 +18,7 @@
             </li>
 
             <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="rh-blurb mx1 my2">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579">
                     <div style="height: 200px; overflow:hidden;position:relative">
                         <img src="{!! $myBlurbs[1]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
@@ -30,7 +30,7 @@
             </li>
 
             <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="rh-blurb mx1 my2">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579">
                     <div style="height: 200px; overflow:hidden;position:relative">
                         <img src="{!! $myBlurbs[2]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
