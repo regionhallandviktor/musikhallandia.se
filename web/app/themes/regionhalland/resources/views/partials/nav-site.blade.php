9125ac480dd87b2aacf46e8d6ecb21894b62cb95
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-16 09:11:34 +0200

    Changes to site head

--- web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/nav-site.blade.php
@@ -1,18 +1,20 @@
-<nav class="rh-header center px3" aria-label="Sidhuvud" style="max-width: 1440px;">
-    <span class="h1"><strong>Yrkesutbildning i Halland</strong></span>
-    {{-- Utilities --}}
-    <div class="rh-toplinks">
-        <ul class="rh-toplinks-nav" aria-label="Genvägar i sidhuvudet">
-            <li class="rh-toplinks-nav-item">
-                <i class="feather icon-volume-1 rh-toplinks-nav-link-icon"></i>
-                <a id="bapluslogo" class="rh-toplinks-nav-link logo" title="Aktivera Talande Webb" onclick="toggleBar();" href="#">Talande Webb</a>
-            </li>
-            <li class="rh-toplinks-nav-item">
-                <i class="feather icon-volume-1 rh-toplinks-nav-link-icon"></i>
-                <a class="rh-toplinks-nav-link logo" href="./om-oss">Om oss</a>
-            </li>
-        </ul>
-    </div>
+<div style="background: #FBFAF9;">
+    <nav class="rh-header center px3" aria-label="Sidhuvud" style="max-width: 1440px; background: #FBFAF9;">
+        <span class="h1"><strong><a style="text-decoration:none; color:black;" href="/">Yrkesutbildning i Halland</a></strong></span>
+        {{-- Utilities --}}
+        <div class="rh-toplinks" style="background: #FBFAF9;">
+            <ul class="rh-toplinks-nav" aria-label="Genvägar i sidhuvudet">
+                <li class="rh-toplinks-nav-item">
+                    <i class="feather icon-volume-1 rh-toplinks-nav-link-icon"></i>
+                    <a id="bapluslogo" class="rh-toplinks-nav-link logo" title="Aktivera Talande Webb" onclick="toggleBar();" href="#">Talande Webb</a>
+                </li>
+                <li class="rh-toplinks-nav-item">
+                    <i class="feather icon-volume-1 rh-toplinks-nav-link-icon"></i>
+                    <a class="rh-toplinks-nav-link logo" href="./om-oss">Om oss</a>
+                </li>
+            </ul>
+        </div>
 
-    {{-- Utilities END --}}
-</nav>
+        {{-- Utilities END --}}
+    </nav>
+</div>
