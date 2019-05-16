72287390867fd8692125bf766182cc42406f5d28
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-16 09:12:45 +0200

    Tweaking top level template

--- web/app/themes/regionhalland/resources/views/layouts/app.blade.php
+++ web/app/themes/regionhalland/resources/views/layouts/app.blade.php
@@ -1,21 +1,12 @@
 <!doctype html>
-
 <html style="height: 101%;" @php(language_attributes())>
-
-@include('partials.head')
-
-<body style="height: 101%" @php(body_class())>
-
-@include('partials.jump-to-content')
-@include('partials.cookie-notice')
-
-@include('partials.header')
-@include('partials.hero')
-
-@yield('content')
-
-@include('partials.footer')
-
-</body>
-
+    @include('partials.head')
+    <body style="height: 101%" @php(body_class())>
+        @include('partials.jump-to-content')
+        @include('partials.cookie-notice')
+        @include('partials.header')
+        @include('partials.hero')
+        @yield('content')
+        @include('partials.footer')
+    </body>
 </html>
