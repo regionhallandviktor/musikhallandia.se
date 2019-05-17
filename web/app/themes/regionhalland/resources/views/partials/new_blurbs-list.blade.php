1bf2148c82333a463e4de1180f7d97231aa9ebab
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-17 11:49:03 +0200

    Redoing blurbs with the logic from the ones on the front page instead

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -1,16 +1,16 @@
 @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
-@if(isset($myBlurbs))
-    <div class="center" style="max-width: 1440px;">
-        <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
+@if($myBlurbs)
+    <div class="mx-auto pt2 pb2" style="background: #F0EEEC; max-width: 1440px;">
+        <ul class="clearfix" style="max-width:1440px;">
         @foreach ($myBlurbs as $blurbs)
-            <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4" style="max-width: 416px;">
-                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579;">
-                    <div style="height: 195px; overflow:hidden;position:relative;">
-                        <img src="{!! $blurbs['image_url'] !!}" style="height:100%;">
+            <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4" >
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background-color: white;">
+                    <div style="height: 200px; overflow:hidden;position:relative">
+                        <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
-                    <div class="rh-blurb__description mx2 pt2">
-                        <h3 style="line-height: 1.3"><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="  color:black;">{{ $blurbs['link_title'] }}</a></h3>
-                        <p style="line-height: 1.4">{{ $blurbs['post_content'] }}</p>
+                    <div class="mx2 pt2 rh-blurb__description" >
+                        <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}" style="color:black">{{ $blurbs['link_title'] }}</a></h3>
+                        <p>{{ $blurbs['post_content'] }}</p>
                     </div>
                 </div>
             </li>
