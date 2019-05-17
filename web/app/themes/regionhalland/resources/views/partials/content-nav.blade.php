2d5f5ee7ca2408f0ab759d7107d3e6ac93fa03d1
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-17 11:47:14 +0200

    Content nav blade from regionhalland.se added

--- web/app/themes/regionhalland/resources/views/partials/content-nav.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/content-nav.blade.php
@@ -3,18 +3,21 @@
 	@if(isset($myNavs) && count($myNavs) > 0)
 		@php($id = uniqid())
 		<div id="content-nav-placeholder"></div>
-		<nav class="content-nav-container rh-get-sticky" id="content-nav-container">
-			<div class="content-nav">
-				<h4 id="{{ $id }}">Hitta på sidan</h4>
-				<ul class="content-nav__list" itemscope itemtype="http://schema.org/ItemList" aria-labelledby="{{ $id }}">
+		<nav class="content-nav-container rh-get-sticky hidden-sm" id="content-nav-container">
+			<div>
+				<p class="h2" id="{{ $id }}">Hitta på sidan</p>
+				<ul>
 					@foreach ($myNavs as $myNav)
-						<li class="content-nav__item" itemprop="itemListElement">
-							<a class="content-nav__link" href="#{{ $myNav['slug'] }}" data-pointstoid="{{ $myNav['slug'] }}">{!! $myNav['content'] !!}</a>
+						<li class="content-nav__item {{ $myNav['class'] }}" >
+							<a class="content-nav__item-link" href="#{{ $myNav['slug'] }}" data-pointstoid="{{ $myNav['slug'] }}">
+								{!! $myNav['content'] !!}
+							</a>
 							<meta itemprop="position" content="{{ $loop->iteration }}" />
 						</li>
 					@endforeach
 				</ul>
 			</div>
+			<div id="content-nav-bottom-placeholder"></div>
 		</nav>
 	@endif
 @endif
