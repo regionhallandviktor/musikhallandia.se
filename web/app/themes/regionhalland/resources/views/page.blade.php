db63ebd65cb66be622911f8351c77efd11991598
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-17 11:46:38 +0200

    Getting layout of regular page done

--- web/app/themes/regionhalland/resources/views/page.blade.php
+++ web/app/themes/regionhalland/resources/views/page.blade.php
@@ -2,8 +2,40 @@
 
 @section('content') 	
 
+	<div class="rh-xpad-A pt3 pb3 clearfix center" style="max-width: 1440px;background: #FBFAF9">
+		<main class="rh-xpad-B pt3 pb3" style="background: white;">
 
-	
+			{{-- Back button component: --}}
+			@php($myParentPage = get_region_halland_parent_page())
+				<div class="pb3 rh-label-previous">
+					<span class="rh-label-previous-icon"></span>
+					<p class="rh-label-previous-title">
+						@if($myParentPage['has_back'] == 1)
+							<a href="{{$myParentPage['url']}}" style="color:black;">{{$myParentPage['post_title']}}</a>
+						@else
+							<a href="/" style="color:black;">Startsidan</a>
+						@endif
+					</p>
+				</div>
+
+			<div class="clearfix">
+				<div class="col col-12 md-col-10">
+					<h1>{{ $post->post_title }}</h1>
+
+					{{ the_content() }}
+				</div>
+				<div class="col col-12 md-col-2">
+					@include('partials.content-nav')
+				</div>
+			</div>
+
+		</main>
+
+
+
+	</div>
+
+	{{--
 	<!-- ************ -->
 	<!-- Page content -->
 	<!-- ************ -->
@@ -11,27 +43,27 @@
 		<div class="background-white">
 			<div class="container mx-auto p4">
 				<div class="m2 flex flex-wrap">
-					
+
 					<div class="col-12 lg-col-3">
-					
+
 						@include('partials.nav-sidebars')
-						
+
 						<div class="pt2">&nbsp;</div>
 
 						@include('partials.content-nav')
-					
+
 					</div>
-					
+
 					<!-- ************ -->
 					<!-- Page content -->
 					<!-- ************ -->
 					<div class="col-12 lg-col-9">
 						<main class="ml4">
-							
+
 							<div>
 								<h1>{{ the_title() }}</h1>
 							</div>
-							
+
 							<div id="main">
 								@while(have_posts()) @php(the_post())
 									@include('partials.article')
@@ -44,7 +76,7 @@
 
 				</div>
 			</div>
-		</div>	
-	</div>
+		</div>
+	</div> --}}
 
 @endsection
