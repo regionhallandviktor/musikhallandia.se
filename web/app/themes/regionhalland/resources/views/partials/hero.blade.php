cefee98dcbb752e32c908a7ae2234f30a24228c5
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-17 14:59:10 +0200

    New hero caption style implemented

--- web/app/themes/regionhalland/resources/views/partials/hero.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/hero.blade.php
@@ -1,7 +1,7 @@
 @if(get_the_post_thumbnail_url())
     <div class="center rh-hero">
         <img class="rh-image-hero" src="{{ get_the_post_thumbnail_url() }}" alt="">
-        <div class="rh-caption-hero">
+        <div class="rh-caption-hero rh-caption-hero--secondary rh-caption-hero--rounded">
             {{ $post->post_title }}
         </div>
     </div>
