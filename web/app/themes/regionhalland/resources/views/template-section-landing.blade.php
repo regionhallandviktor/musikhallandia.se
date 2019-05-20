8398da2c67267a61427673c69d701ccc872b3043
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-20 16:12:11 +0200

    Back button added

--- web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
+++ web/app/themes/regionhalland/resources/views/template-section-landing.blade.php
@@ -7,6 +7,17 @@
 @extends('layouts.app')
 
 @section('content')
+    @php($myParentPage = get_region_halland_parent_page())
+    <div class="rh-xpad-B py2">
+        @if($myParentPage['has_back'] == 1)
+            <a href="{{$myParentPage['url']}}" class="rh-round-button--vuxhalland rh-round-button icon-arrow-left"></a>
+            <a href="{{$myParentPage['url']}}" class="pl1 rh-link--navigation">{{$myParentPage['post_title']}}</a>
+
+        @else
+            <a href="/" class="rh-round-button--vuxhalland rh-round-button icon-arrow-left"></a>
+            <a href="/" class="pl1 rh-link--navigation">Startsidan</a>
+        @endif
+    </div>
     @include('partials.new_blurbs-list')
     @include('partials.section-navigation')
 @endsection
