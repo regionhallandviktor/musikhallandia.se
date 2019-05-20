1e861870f6a552d809c281ed52fac199f73be3d7
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-20 16:09:21 +0200

    Responsive behaviour

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -1,7 +1,7 @@
 @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
 @if($myBlurbs)
     <div class="mx-auto pt2 pb2" style="background: #F0EEEC; max-width: 1440px;">
-        <ul class="clearfix" style="max-width:1440px;">
+        <ul class="clearfix center pb3 pt3 rh-xpad-B">
         @foreach ($myBlurbs as $blurbs)
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4" >
                 <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background-color: white;">
@@ -9,7 +9,7 @@
                         <img src="{!! $blurbs['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="mx2 pt2 rh-blurb__description" >
-                        <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}"  class="rh-link--navigation">{{ $blurbs['link_title'] }}</a></h3>
+                        <h3><a href="{{ $blurbs['link_url'] }}" target="{{ $blurbs['link_target'] }}"  class="rh-link--navigation"0>{{ $blurbs['link_title'] }}</a></h3>
                         <p>{{ $blurbs['post_content'] }}</p>
                     </div>
                 </div>
