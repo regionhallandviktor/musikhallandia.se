38ae4530f36bc99fc583d9c77078a797fc9c9f5f
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-20 16:08:00 +0200

    Limiting breadcrumb container width and removing full page width background

--- web/app/themes/regionhalland/resources/views/partials/breadcrumbs.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/breadcrumbs.blade.php
@@ -29,7 +29,7 @@
 @if(!is_front_page())
 	@php($breadcrumbs = get_region_halland_breadcrumbs_pages())
 	@if(isset($breadcrumbs))
-		<div class="rh-breadcrumbs hidden-sm">
+		<div class="rh-breadcrumbs hidden-sm mx-auto" style="max-width:1440px;">
 			<ul class="rh-breadcrumb-group pl3 mx-auto" style="max-width:1440px;">
 				@foreach ($breadcrumbs as $breadcrumb)
 					<li class="rh-breadcrumb-group-item" @if ($loop->last) aria-current="page" @endif itemscope itemprop="itemListElement" itemtype="http://schema.org/ListItem">
