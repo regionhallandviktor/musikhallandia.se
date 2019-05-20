7b32537d35a39dd15d6c785af35dfd14561d8dad
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-20 14:26:33 +0200

    Using new rh-link--navigation from styleguide 4.0.0

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -11,7 +11,7 @@
                         <img src="{!! $myBlurbs[0]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="mx2 pt2 rh-blurb__description">
-                        <h3><a href="{{ $myBlurbs[0]['link_url'] }}" target="{{ $myBlurbs[0]['link_target'] }}" style="color:black">{{ $myBlurbs[0]['link_title'] }}</a></h3>
+                        <h3><a href="{{ $myBlurbs[0]['link_url'] }}" class="rh-link--navigation" target="{{ $myBlurbs[0]['link_target'] }}">{{ $myBlurbs[0]['link_title'] }}</a></h3>
                         <p>{{ $myBlurbs[0]['post_content'] }}</p>
                     </div>
                 </div>
@@ -23,7 +23,7 @@
                         <img src="{!! $myBlurbs[1]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="rh-blurb__description mx2 pt2" >
-                        <h3><a href="{{ $myBlurbs[1]['link_url'] }}" target="{{ $myBlurbs[1]['link_target'] }}" style="color:black">{{ $myBlurbs[1]['link_title'] }}</a></h3>
+                        <h3><a href="{{ $myBlurbs[1]['link_url'] }}" class="rh-link--navigation" target="{{ $myBlurbs[1]['link_target'] }}">{{ $myBlurbs[1]['link_title'] }}</a></h3>
                         <p>{{ $myBlurbs[1]['post_content'] }}</p>
                     </div>
                 </div>
@@ -35,7 +35,7 @@
                         <img src="{!! $myBlurbs[2]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
                     <div class="mx2 pt2 rh-blurb__description">
-                        <h3><a href="{{ $myBlurbs[2]['link_url'] }}" target="{{ $myBlurbs[2]['link_target'] }}" style="color:black">{{ $myBlurbs[2]['link_title'] }}</a></h3>
+                        <h3><a href="{{ $myBlurbs[2]['link_url'] }}" class="rh-link--navigation" target="{{ $myBlurbs[2]['link_target'] }}">{{ $myBlurbs[2]['link_title'] }}</a></h3>
                         <p>{{ $myBlurbs[2]['post_content'] }}</p>
                     </div>
                 </div>
@@ -52,7 +52,7 @@
                 <li class="rh-navigation-card left-align col-12 sm-col-6 md-col-4 lg-col-3 pr2" style="position:relative">
                     <div class="rh-navigation-card-title">
                         <a href="{{ $page->url }}" class="rh-round-button rh-round-button--small rh-round-button--vuxhalland icon-arrow-right"></a>
-                        <strong><a href="{{ $page->url }}" class="h3" style="color:black; text-decoration: none;">
+                        <strong><a href="{{ $page->url }}" class="h3 rh-link--navigation">
                                 {{ $page->post_title }}
                             </a></strong>
                     </div>
