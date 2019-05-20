58a8bef722b11aca52601413f73fe49aa88ac0ec
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-20 16:07:06 +0200

    New look regarding blurbs

--- web/app/themes/regionhalland/resources/views/front-page.blade.php
+++ web/app/themes/regionhalland/resources/views/front-page.blade.php
@@ -3,14 +3,14 @@
     @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
     @if(isset($myBlurbs))
 
-        <ul class="clearfix center mb2 mt2" style="max-width:1440px;">
-
+        <ul class="clearfix center pb3 pt3 rh-xpad-B" style="max-width:1440px;background: #F0EEEC;">
+<h1>Vilken utbildning passar dig?</h1>
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background:white;">
                     <div style="height: 200px; overflow:hidden;position:relative">
                         <img src="{!! $myBlurbs[0]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
-                    <div class="mx2 pt2 rh-blurb__description">
+                    <div class="mx2 pt2 rh-blurb__description" >
                         <h3><a href="{{ $myBlurbs[0]['link_url'] }}" class="rh-link--navigation" target="{{ $myBlurbs[0]['link_target'] }}">{{ $myBlurbs[0]['link_title'] }}</a></h3>
                         <p>{{ $myBlurbs[0]['post_content'] }}</p>
                     </div>
@@ -18,7 +18,7 @@
             </li>
 
             <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background: white; ">
                     <div style="height: 200px; overflow:hidden;position:relative">
                         <img src="{!! $myBlurbs[1]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
@@ -30,7 +30,7 @@
             </li>
 
             <li class="left-align  col col-12 sm-col-6 md-col-6 lg-col-4">
-                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579">
+                <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background: white;">
                     <div style="height: 200px; overflow:hidden;position:relative">
                         <img src="{!! $myBlurbs[2]['image_url'] !!}" style="width:100%; position: absolute; top:50%; transform: translateY(-50%);">
                     </div>
@@ -41,6 +41,7 @@
                 </div>
             </li>
         </ul>
+
     @endif
 
 
