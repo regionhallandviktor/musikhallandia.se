d75f401cb71e5236e1d8cb6d14b0df3114988ae9
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-22 10:24:25 +0200

    Colour adjustments

--- web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/new_blurbs-list.blade.php
@@ -1,7 +1,7 @@
 @php($myBlurbs = get_region_halland_acf_main_post_page_links_blurbs())
 @if($myBlurbs)
-    <div class="mx-auto pt2 pb2" style="background: #F0EEEC; max-width: 1440px;">
-        <ul class="clearfix center pb3 pt3 rh-xpad-B">
+    <div class="mx-auto pt2 pb2" style="background: #F8F6F4; max-width: 1440px;">
+        <ul class="clearfix center pb3 pt3">
         @foreach ($myBlurbs as $blurbs)
             <li class="left-align col col-12 sm-col-6 md-col-6 lg-col-4" >
                 <div class="rh-blurb mx1 my2" style="border-bottom: 4px solid #E99579; background-color: white;">
