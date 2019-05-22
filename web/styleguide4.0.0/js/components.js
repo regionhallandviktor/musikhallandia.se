f3041268ed897b5bcb1fa6e77121c20632bf665d
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-22 10:24:50 +0200

    Styleguide 4.0.0

--- web/styleguide4.0.0/js/components.js
+++ web/styleguide4.0.0/js/components.js
@@ -43,50 +43,6 @@ window.onclick = function (event) {
 "use strict";
 "use strict";
 
-var acc = document.getElementsByClassName("rh-accordion");
-var i;
-
-for (i = 0; i < acc.length; i++) {
-  acc[i].addEventListener("click", function () {
-    this.classList.toggle("rh-accordion-active");
-    var panel = this.nextElementSibling;
-
-    if (panel.style.maxHeight) {
-      panel.style.maxHeight = null;
-    } else {
-      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
-    }
-  });
-}
-"use strict";
-
-var videoPlayButton,
-    videoWrapper = document.getElementsByClassName('rh-video')[0],
-    video = document.getElementsByTagName('video')[0],
-    videoMethods = {
-  renderVideoPlayButton: function renderVideoPlayButton() {
-    if (videoWrapper !== undefined && videoWrapper.contains(video)) {
-      this.formatVideoPlayButton();
-      video.classList.add('has-media-controls-hidden');
-      videoPlayButton = document.getElementsByClassName('video-button')[0];
-      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
-    }
-  },
-  formatVideoPlayButton: function formatVideoPlayButton() {
-    videoWrapper.insertAdjacentHTML('beforeend', '\
-            <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
-            ');
-  },
-  hideVideoPlayButton: function hideVideoPlayButton() {
-    video.play();
-    videoPlayButton.classList.add('is-hidden');
-    video.classList.remove('has-media-controls-hidden');
-    video.setAttribute('controls', 'controls');
-  }
-};
-videoMethods.renderVideoPlayButton();
-"use strict";
-
 var acc = document.getElementsByClassName("rh-search-accordion");
 var i;
 
@@ -197,3 +153,47 @@ function setShadow() {
     }
   }
 }
+"use strict";
+
+var acc = document.getElementsByClassName("rh-accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("rh-accordion-active");
+    var panel = this.nextElementSibling;
+
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + 100 + "px";
+    }
+  });
+}
+"use strict";
+
+var videoPlayButton,
+    videoWrapper = document.getElementsByClassName('rh-video')[0],
+    video = document.getElementsByTagName('video')[0],
+    videoMethods = {
+  renderVideoPlayButton: function renderVideoPlayButton() {
+    if (videoWrapper !== undefined && videoWrapper.contains(video)) {
+      this.formatVideoPlayButton();
+      video.classList.add('has-media-controls-hidden');
+      videoPlayButton = document.getElementsByClassName('video-button')[0];
+      videoPlayButton.addEventListener('click', this.hideVideoPlayButton);
+    }
+  },
+  formatVideoPlayButton: function formatVideoPlayButton() {
+    videoWrapper.insertAdjacentHTML('beforeend', '\
+            <div class="video-button"><div class="video-button-content"> <i class="feather icon-play"></i> </div></div>\
+            ');
+  },
+  hideVideoPlayButton: function hideVideoPlayButton() {
+    video.play();
+    videoPlayButton.classList.add('is-hidden');
+    video.classList.remove('has-media-controls-hidden');
+    video.setAttribute('controls', 'controls');
+  }
+};
+videoMethods.renderVideoPlayButton();
