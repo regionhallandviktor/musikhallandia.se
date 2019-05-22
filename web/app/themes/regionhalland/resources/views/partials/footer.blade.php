ce8d0b3e8534338e9758ae5de4bc513b1131e532
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-22 10:23:02 +0200

    Linked the logos of the footer

--- web/app/themes/regionhalland/resources/views/partials/footer.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/footer.blade.php
@@ -1,84 +1,26 @@
 <footer class="rh-footer-logo mx-auto mt1" id="footer-top-placeholder" style="max-width: 1440px;">
 
     <div class="rh-footer-logo-div">
-        <img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/RegionHalland.png" alt="RegionHalland Logo">
+        <a href="https://www.regionhalland.se"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/RegionHalland.png" alt="RegionHalland Logo"></a>
     </div>
     <div class="rh-footer-logo-div">
-        <img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Falkenberg.png" alt="Falkenberg Logo">
+        <a href="https://www.falkenberg.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Falkenberg.png" alt="Falkenberg Logo"></a>
     </div>
     <div class="rh-footer-logo-div">
-        <img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Halmstad.png" alt="Halmstad Logo">
+        <a href="https://www.halmstad.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Halmstad.png" alt="Halmstad Logo"></a>
     </div>
     <div class="rh-footer-logo-div">
-        <img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Hylte.png" alt="Hylte Logo">
+        <a href="http://hylte.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Hylte.png" alt="Hylte Logo"></a>
     </div>
     <div class="rh-footer-logo-div">
-        <img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Laholm.png" alt="Laholm Logo">
+        <a href="https://www.laholm.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Laholm.png" alt="Laholm Logo"></a>
     </div>
     <div class="rh-footer-logo-div">
-        <img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Varberg.png" alt="Varberg Logo">
+        <a href="https://varberg.se/"><img class="rh-footer-logo-item" src="{!! env('WP_HOME') !!}/styleguide4.0.0/images/components/kommuner_logo/Varberg.png" alt="Varberg Logo"></a>
     </div>
 
 </footer>
 
-{{--
-<div class="rh-footer">
-
-	<div class="rh-footer-section">
-		<h5>Kontakta oss </h5>
-		<hr class="rh-footer-title-hr">
-		<p>
-			Region Halland </br>
-			Box 517 </br>
-			301 80 Halmstad
-		</p>
-		<p><strong> Telefon:</strong> 035 - 13 48 00 </p>
-		<p><strong> E-post:</strong> <a class="rh-link"> regionen@regionhalland.se</a> </p>
-		<br>
-		<p><a class="rh-link"> Kontakta mottagning</a></p>
-		<p><a class="rh-link"> Kontakta personal</a></p>
-	</div>
-
-	<div class="rh-footer-section">
-		<h5>Övriga webbplatser </h5>
-		<hr class="rh-footer-title-hr">
-		<ul>
-			<li class="rh-footer-item">
-				<a class="rh-link" href="#">
-					Regionhalland.se
-				</a>
-			</li>
-			<li class="rh-footer-item">
-				<a class="rh-link" href="#">
-					Vardgivare.regionhalland.se
-				</a>
-			</li>
-			<li class="rh-footer-item">
-				<a class="rh-link" href="#">
-					Intra.regionhalland.se
-				</a>
-			</li>
-			<li class="rh-footer-item">
-				<a class="rh-link" href="#">
-					Hallandstrafiken.se
-				</a>
-			</li>
-			<li class="rh-footer-item">
-				<a class="rh-link" href="#">
-					1177.se
-				</a>
-			</li>
-		</ul>
-	</div>
-
-	<div class="rh-footer-section">
-		<h5>Om webbplatsen </h5>
-		<hr class="rh-footer-title-hr">
-		<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation allamco laboris nisi ut aliquip ex ea commodo consequat. </p>
-	</div>
-
-</div>
- --}}
 <script src="{!! env('WP_HOME') !!}/include/scripts/jquery.3.3.1.min.js?ver=3.3.1"></script>
 
 <script src="{!! env('WP_HOME') !!}/styleguide4.0.0/js/components.js"></script>
