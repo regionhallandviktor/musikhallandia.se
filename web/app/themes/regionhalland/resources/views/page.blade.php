d36038f5b49127a565bdd182b8a2caf3430bc1ba
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-24 13:00:07 +0200

    Fixing IE bug with main tag not handled properly

--- web/app/themes/regionhalland/resources/views/page.blade.php
+++ web/app/themes/regionhalland/resources/views/page.blade.php
@@ -3,37 +3,39 @@
 @section('content') 	
 
 	<div class="rh-xpad-A pt3 pb3 clearfix center" style="max-width: 1440px;background: #FBFAF9">
-		<main class="rh-xpad-B pt3 pb3 rh-article" style="background: white;">
+		<div class="rh-xpad-B pt3 pb3 rh-article" style="background: white;">
+			<main>
 
-			{{-- Back button component: --}}
-			@php($myParentPage = get_region_halland_parent_page())
-				<div class="pb3">
-					@if($myParentPage['has_back'] == 1)
-						<a href="{{$myParentPage['url']}}" class="rh-round-button--vuxhalland rh-round-button icon-arrow-left"></a>
-						<a href="{{$myParentPage['url']}}" class="pl1 rh-link--navigation">{{$myParentPage['post_title']}}</a>
+				{{-- Back button component: --}}
+				@php($myParentPage = get_region_halland_parent_page())
+					<div class="pb3">
+						@if($myParentPage['has_back'] == 1)
+							<a href="{{$myParentPage['url']}}" class="rh-round-button--vuxhalland rh-round-button icon-arrow-left"></a>
+							<a href="{{$myParentPage['url']}}" class="pl1 rh-link--navigation">{{$myParentPage['post_title']}}</a>
 
-					@else
-						<a href="/" class="rh-round-button--vuxhalland rh-round-button icon-arrow-left"></a>
-						<a href="/" class="pl1 rh-link--navigation">Startsidan</a>
-					@endif
+						@else
+							<a href="/" class="rh-round-button--vuxhalland rh-round-button icon-arrow-left"></a>
+							<a href="/" class="pl1 rh-link--navigation">Startsidan</a>
+						@endif
+					</div>
+
+				<div class="clearfix">
+					<div class="col col-12 md-col-9">
+						@while(have_posts()) @php(the_post())
+						<h1>{{ $post->post_title }}</h1><br><br>
+						@if(function_exists('get_region_halland_prepare_the_content'))
+							@php(get_region_halland_prepare_the_content())
+						@endif
+						{{ the_content() }}
+						@endwhile
+					</div>
+					<div class="pl4 col col-12 md-col-3">
+						@include('partials.content-nav')
+					</div>
 				</div>
 
-			<div class="clearfix">
-				<div class="col col-12 md-col-9">
-					@while(have_posts()) @php(the_post())
-					<h1>{{ $post->post_title }}</h1><br><br>
-					@if(function_exists('get_region_halland_prepare_the_content'))
-						@php(get_region_halland_prepare_the_content())
-					@endif
-					{{ the_content() }}
-					@endwhile
-				</div>
-				<div class="pl4 col col-12 md-col-3">
-					@include('partials.content-nav')
-				</div>
-			</div>
-
-		</main>
+			</main>
+		</div>
 
 
 
