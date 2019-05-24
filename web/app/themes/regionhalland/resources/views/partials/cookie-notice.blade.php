87478b229065481be3f70afa1ae87885a294ba8a
Viktor Sarge <viktor.sarge@regionhalland.se>
2019-05-24 13:08:59 +0200

    New cookie notice colours

--- web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
+++ web/app/themes/regionhalland/resources/views/partials/cookie-notice.blade.php
@@ -1,11 +1,11 @@
 @php($checkCookieNotice = check_region_halland_cookie_notice())
 @if ($checkCookieNotice == false)
 	@php($cookie_notice = get_region_halland_cookie_notice())
-	<div class="rh-cookie" style="background-color: rgba(195,220,193,0.5);">
+	<div class="rh-cookie" style="background-color: #E4E4E4;">
 		<span class="rh-cookie-icon"></span>
 		<p class="rh-cookie-description">
 			{!! $cookie_notice['message'] !!}
 		</p>
-		<button id="cookie-consent" style="background-color: #378A30;width:13ex;" class="rh-button rh-button--primary cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
+		<button id="cookie-consent" style="width:13ex;" class="rh-button cookie-button" aria-label="Acceptera cookies och dölj meddelandet" role="button">{!! $cookie_notice['button'] !!}</button>
 	</div>
 @endif
